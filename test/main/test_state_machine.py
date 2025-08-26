8f539e99ade580d9d9dbba36200cf245ca59154f
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-08-26 14:17:07 +0100

    Add python tests for the state machine
    While there are already Playwright tests and the state machine
    provides several correctness guarantees[^1], these additional
    tests will confirm that the correct states and routes are returned
    following a given transition
    
    [^1]: https://python-statemachine.readthedocs.io/en/latest/readme.html

---
+++ test/main/test_state_machine.py
@@ -0,0 +1,37 @@
+import pytest
+from types import SimpleNamespace
+from app.constants import MultiPageFormRoutes
+from app.lib.state_machine.state_machine import RoutingStateMachine
+
+
+def test_initial_state_has_no_route():
+    sm = RoutingStateMachine()
+    assert sm.current_state.id == "initial"
+    assert sm.route_for_current_state is None
+
+
+def test_continue_to_service_person_alive_form_sets_route():
+    sm = RoutingStateMachine()
+    sm.continue_to_service_person_alive_form()
+    assert sm.current_state.id == "service_person_alive_form"
+    assert sm.route_for_current_state == MultiPageFormRoutes.IS_SERVICE_PERSON_ALIVE.value
+
+
+@pytest.mark.parametrize(
+    "answer,expected_state,expected_route",
+    [
+        ("yes", "subject_access_request_statement", MultiPageFormRoutes.MUST_SUBMIT_SUBJECT_ACCESS_REQUEST.value),
+        ("no", "service_branch_form", MultiPageFormRoutes.SERVICE_BRANCH_FORM.value),
+    ],
+)
+def test_continue_from_service_person_alive_form_routes_by_condition(answer, expected_state, expected_route):
+    sm = RoutingStateMachine()
+    sm.continue_from_service_person_alive_form(form=make_form(answer))
+    assert sm.current_state.id == expected_state
+    assert sm.route_for_current_state == expected_route
+
+
+def make_form(answer: str):
+    return SimpleNamespace(
+        is_service_person_alive=SimpleNamespace(data=answer)
+    )
