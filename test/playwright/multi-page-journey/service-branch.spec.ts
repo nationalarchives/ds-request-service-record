fa7497d80f8927fe4c86ff401233c70b577d6635
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-08-28 14:10:43 +0100

    FOI-47: Refactor Playwright tests
    Replaces multiple const declaration with an Enum which
    uses template strings for values.

--- test/playwright/multi-page-journey/service-branch.spec.ts
+++ test/playwright/multi-page-journey/service-branch.spec.ts
@@ -1,16 +1,19 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("What was the person's service branch?", () => {
-  const JOURNEY_START_PAGE_URL = "/request-a-service-record/start/";
-  const SERVICE_BRANCH_URL = "/request-a-service-record/service-branch/";
-  const WAS_SERVICE_PERSON_OFFICER_URL =
-    "/request-a-service-record/was-service-person-officer/";
-  const MOD_HAVE_THIS_RECORD_URL = "/request-a-service-record/mod-have-this-record/";
-  const CHECK_ANCESTRY_URL = "/request-a-service-record/check-ancestry/";
+  const basePath = "/request-a-service-record";
+
+  enum Urls {
+    JOURNEY_START_PAGE = `${basePath}/start/`,
+    SERVICE_BRANCH = `${basePath}/service-branch/`,
+    WAS_SERVICE_PERSON_OFFICER = `${basePath}/was-service-person-officer/`,
+    MOD_HAVE_THIS_RECORD = `${basePath}/mod-have-this-record/`,
+    CHECK_ANCESTRY = `${basePath}/check-ancestry/`,
+  }
 
   test.beforeEach(async ({ page }) => {
-    await page.goto(JOURNEY_START_PAGE_URL); // We need to go here first because we prevent direct access to mid-journey pages
-    await page.goto(SERVICE_BRANCH_URL);
+    await page.goto(Urls.JOURNEY_START_PAGE); // We need to go here first because we prevent direct access to mid-journey pages
+    await page.goto(Urls.SERVICE_BRANCH);
   });
 
   test("Shows the correct heading", async ({ page }) => {
@@ -31,35 +34,35 @@ test.describe("What was the person's service branch?", () => {
   [
     {
       branchLabel: "British Army",
-      nextUrl: WAS_SERVICE_PERSON_OFFICER_URL,
+      nextUrl: Urls.WAS_SERVICE_PERSON_OFFICER,
       expectedHeading: /Was the service person an officer\?/,
     },
     {
       branchLabel: "Royal Navy",
-      nextUrl: MOD_HAVE_THIS_RECORD_URL,
+      nextUrl: Urls.MOD_HAVE_THIS_RECORD,
       expectedHeading: /The Ministry of Defence has this record/,
     },
     {
       branchLabel: "Royal Air Force",
-      nextUrl: WAS_SERVICE_PERSON_OFFICER_URL,
+      nextUrl: Urls.WAS_SERVICE_PERSON_OFFICER,
       expectedHeading: /Was the service person an officer\?/,
     },
     {
       branchLabel: "I don't know",
-      nextUrl: WAS_SERVICE_PERSON_OFFICER_URL,
+      nextUrl: Urls.WAS_SERVICE_PERSON_OFFICER,
       expectedHeading: /Was the service person an officer\?/,
     },
     {
       branchLabel: "Home Guard",
-      nextUrl: CHECK_ANCESTRY_URL,
+      nextUrl: Urls.CHECK_ANCESTRY,
       expectedHeading: /Check Ancestry/,
     },
   ].forEach(({ branchLabel, nextUrl, expectedHeading }) => {
     test(`Presents the correct form when ${branchLabel} is selected`, async ({
       page,
     }) => {
-      await page.goto(JOURNEY_START_PAGE_URL);
-      await page.goto(SERVICE_BRANCH_URL);
+      await page.goto(Urls.JOURNEY_START_PAGE);
+      await page.goto(Urls.SERVICE_BRANCH);
       await page.getByLabel(branchLabel, { exact: true }).check();
       await page.getByRole("button", { name: /Continue/i }).click();
       await expect(page).toHaveURL(nextUrl);
