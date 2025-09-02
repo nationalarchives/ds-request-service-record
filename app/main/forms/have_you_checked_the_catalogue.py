322391e1f667fa34e6bfd0214cb549eba2f8e205
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-09-02 12:17:48 +0100

    FOI-57: Add HaveYouCheckedTheCatalogue FlaskForm
    This uses content from the YAML and will be passed to the route function.

---
+++ app/main/forms/have_you_checked_the_catalogue.py
@@ -0,0 +1,33 @@
+from app.constants import ServiceBranches
+from app.lib.content import get_field_content, load_content
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, FileSize
+from tna_frontend_jinja.wtforms import (
+    TnaRadiosWidget,
+    TnaSubmitWidget,
+)
+from tna_frontend_jinja.wtforms import validators as tna_frontend_validators
+from wtforms import (
+    RadioField,
+    SubmitField,
+)
+from wtforms.validators import Email, InputRequired
+
+
+class HaveYouCheckedTheCatalogue(FlaskForm):
+    content = load_content()
+
+    have_you_checked_the_catalogue = RadioField(
+        content["pages"]["have_you_checked_the_catalogue"]["heading"],
+        choices=[("yes", "Yes"), ("no", "No")],
+        validators=[
+            InputRequired(
+                message=get_field_content(content, "have_you_checked_the_catalogue", "messages")[
+                    "required"
+                ]
+            )
+        ],
+        widget=TnaRadiosWidget(),
+    )
+
+    submit = SubmitField(get_field_content(content, "have_you_checked_the_catalogue", "call_to_action"), widget=TnaSubmitWidget())
