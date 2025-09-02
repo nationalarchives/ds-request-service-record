710b02a25bc8427001050ae68d931e6a1262b66b
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-09-02 11:50:38 +0100

    FOI-57: Update Playwright tests
    We're doing two things here:
    
    1. We're updating the existing start page test to ensure the user is
       routed to the new page
    2. We're introducing a new test for the new page, including a check that
       users are kept on the page and shown a validation error when they try
       to submit the form without selecting an option.
    
    Because we are changing where the user is taken upon clicking 'Start now'
    this first commit updates the Playwright tests. At this stage they are failing
    because the state machine has not been updated.

--- test/playwright/multi-page-journey/have-you-checked-the-catalogue.spec.ts
+++ test/playwright/multi-page-journey/have-you-checked-the-catalogue.spec.ts
@@ -1,27 +1,28 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("application start page", () => {
+test.describe("'Have you checked the catalogue?' form", () => {
   const JOURNEY_START_PAGE_URL = "/request-a-service-record/start/";
   const HAVE_YOU_CHECKED_THE_CATALOGUE =
     "/request-a-service-record/have-you-checked-the-catalogue/";
 
   test.beforeEach(async ({ page }) => {
     await page.goto(JOURNEY_START_PAGE_URL);
+    await page.goto(HAVE_YOU_CHECKED_THE_CATALOGUE);
   });
 
   test("Has the right heading", async ({ page }) => {
     await expect(page.locator("h1")).toHaveText(
-      /Request a military service record/,
+      /Have you checked if this record is available in our catalogue\?/,
     );
   });
 
-  test("clicking 'Start now' takes the user to the 'Have you checked the catalogue?' form", async ({
+  test("clicking 'Next' without having made a selection keeps the user on the page and shows a validation error", async ({
     page,
   }) => {
-    await page.getByRole("button", { name: /Start now/i }).click();
+    await page.getByRole("button", { name: /Next/i }).click();
     await expect(page).toHaveURL(HAVE_YOU_CHECKED_THE_CATALOGUE);
-    await expect(page.locator("h1")).toHaveText(
-      /Have you checked if this record is available in our catalogue\?/,
+    await expect(page.locator(".tna-form__error-message")).toHaveText(
+      /Choosing an option is required/,
     );
   });
 });
