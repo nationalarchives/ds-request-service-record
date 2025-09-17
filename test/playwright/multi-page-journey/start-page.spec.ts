e1c4b9fba507cb0aff53d50e4cf1e5aee465d67f
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-09-17 10:13:49 +0100

    Fix: Make back link on 'Have you checked the catalogue?' got to 'Start' page
    I spotted this while introducing the breadcrumbs, so have introduced tests to
    ensure it continues to work as expected.

--- test/playwright/multi-page-journey/start-page.spec.ts
+++ test/playwright/multi-page-journey/start-page.spec.ts
@@ -1,12 +1,13 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("application start page", () => {
-  const JOURNEY_START_PAGE_URL = "/request-a-service-record/start/";
-  const HAVE_YOU_CHECKED_THE_CATALOGUE =
-    "/request-a-service-record/have-you-checked-the-catalogue/";
+  enum Urls {
+    START_PAGE = "/request-a-service-record/start/",
+    HAVE_YOU_CHECKED_THE_CATALOGUE = "/request-a-service-record/have-you-checked-the-catalogue/",
+  }
 
   test.beforeEach(async ({ page }) => {
-    await page.goto(JOURNEY_START_PAGE_URL);
+    await page.goto(Urls.START_PAGE);
   });
 
   test("has the correct heading", async ({ page }) => {
@@ -19,9 +20,18 @@ test.describe("application start page", () => {
     page,
   }) => {
     await page.getByRole("button", { name: /Start now/i }).click();
-    await expect(page).toHaveURL(HAVE_YOU_CHECKED_THE_CATALOGUE);
+    await expect(page).toHaveURL(Urls.HAVE_YOU_CHECKED_THE_CATALOGUE);
     await expect(page.locator("h1")).toHaveText(
       /Have you checked if this record is available in our catalogue\?/,
     );
+  });
+
+  test("clicking 'Back' from 'Have you checked the catalogue?' brings the user back to the start page", async ({
+    page,
+  }) => {
+    await page.getByRole("button", { name: /Start now/i }).click();
+    await expect(page).toHaveURL(Urls.HAVE_YOU_CHECKED_THE_CATALOGUE);
+    await page.getByRole("link", { name: "Back" }).click();
+    await expect(page).toHaveURL(Urls.START_PAGE);
   });
 });
