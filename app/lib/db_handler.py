05959098813fb52291ee9854cbadfc6b41de71c9
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-09-30 16:35:25 +0100

    add/get functions

--- app/lib/db_handler.py
+++ app/lib/db_handler.py
@@ -1,4 +1,4 @@
-from app.lib.models import DynamicsPayment, ServiceRecordRequest, db
+from app.lib.models import DynamicsPayment, GOVUKDynamicsPayment, ServiceRecordRequest, db
 from flask import current_app
 
 
@@ -58,8 +58,17 @@ def delete_service_record_request(record: ServiceRecordRequest) -> None:
         current_app.logger.error(f"Error deleting service record request: {e}")
         db.session.rollback()
 
+def get_dynamics_payment(id: str) -> DynamicsPayment | None:
+    try:
+        payment = db.session.get(DynamicsPayment, id)
+        if not payment:
+            current_app.logger.error(f"Dynamics payment not found for ID: {id}")
+        return payment
+    except Exception as e:
+        current_app.logger.error(f"Error fetching dynamics payment: {e}")
+        return None
 
-def add_dynamics_payment(data: dict) -> None:
+def add_dynamics_payment(data: dict) -> str | None:
     try:
         payment = DynamicsPayment(**data)
         db.session.add(payment)
@@ -69,3 +78,23 @@ def add_dynamics_payment(data: dict) -> None:
         db.session.rollback()
     
     return payment.id
+
+def add_gov_uk_dynamics_payment(data: dict) -> None:
+    try:
+        payment = GOVUKDynamicsPayment(**data)
+        db.session.add(payment)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(f"Error adding GOV.UK dynamics payment: {e}")
+        db.session.rollback()
+
+def get_gov_uk_dynamics_payment(id: str) -> GOVUKDynamicsPayment | None:
+    try:
+        payment = db.session.get(GOVUKDynamicsPayment, id)
+        if not payment:
+            current_app.logger.error(f"GOV.UK dynamics payment not found for ID: {id}")
+        return payment
+    except Exception as e:
+        current_app.logger.error(f"Error fetching GOV.UK dynamics payment: {e}")
+        return None
+    
