4f2ac0246459cc48eb1e3105facedeab6a26e81e
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-10-29 12:32:18 +0000

    Allow _generate_tagged_data to be used for payments or requests

--- app/lib/dynamics_handler.py
+++ app/lib/dynamics_handler.py
@@ -1,8 +1,8 @@
 from app.lib.aws import send_email
-from app.lib.models import ServiceRecordRequest
+from app.lib.models import DynamicsPayment, ServiceRecordRequest
 from flask import current_app
 
-DYNAMICS_FIELD_MAP = [
+DYNAMICS_REQUEST_FIELD_MAP = [
     ("enquiry_id", "id"),
     ("title", "requester_title"),
     ("mandatory_forename", "requester_first_name"),
@@ -35,11 +35,27 @@ DYNAMICS_FIELD_MAP = [
     ("prior_contact_reference", "case_reference_number"),
 ]
 
+DYNAMICS_PAYMENT_FIELD_MAP = [
+    ("payment_id", "id"),
+    ("case_number", "case_number"),
+    ("reference", "reference"),
+    ("net_amount_pence", "net_amount"),
+    ("delivery_amount_pence", "delivery_amount"),
+    ("total_amount_pence", "total_amount"),
+    ("payee_email", "payee_email"),
+    ("first_name", "first_name"),
+    ("last_name", "last_name"),
+    ("details", "details"),
+    ("status", "status"),
+    ("provider_id", "provider_id"),
+    ("created_at", "created_at"),
+]
 
-def send_data_to_dynamics(record: ServiceRecordRequest) -> None:
+
+def send_request_to_dynamics(record: ServiceRecordRequest) -> None:
     # TODO: Check "status" of record, based on defined logic (used in Dynamics email subject, e.g. FOICD, DPA, etc)
 
-    tagged_data = generate_tagged_data(record)
+    tagged_data = generate_tagged_request(record)
 
     send_email(
         to=current_app.config["DYNAMICS_INBOX"],
@@ -48,11 +64,17 @@ def send_data_to_dynamics(record: ServiceRecordRequest) -> None:
     )
 
 
-def generate_tagged_data(record: ServiceRecordRequest) -> str:
+def _generate_tagged_data(mapping: list[tuple[str, str | None]], obj) -> str:
     chunks = []
-    for tag, attr in DYNAMICS_FIELD_MAP:
-        value = getattr(record, attr) if attr else None
+    for tag, attr in mapping:
+        value = getattr(obj, attr) if attr else None
         if value:
             text = str(value)
             chunks.append(f"<{tag}>{text}</{tag}>")
     return "\n".join(chunks)
+
+def generate_tagged_request(record: ServiceRecordRequest) -> str:
+    return _generate_tagged_data(DYNAMICS_REQUEST_FIELD_MAP, record)
+
+def generate_tagged_payment(record: DynamicsPayment) -> str:
+    return _generate_tagged_data(DYNAMICS_PAYMENT_FIELD_MAP, record)
