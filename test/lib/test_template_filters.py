88a600d7e8c6264efd593c34fb65707ea6971d12
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-11-05 16:44:22 +0000

    FOI-127: Add target and rel attributes to link parser

--- test/lib/test_template_filters.py
+++ test/lib/test_template_filters.py
@@ -7,18 +7,18 @@ from app.lib.template_filters import parse_markdown_links
     [
         (
             "My favorite search engine is [Duck Duck Go](https://duckduckgo.com)",
-            'My favorite search engine is <a href="https://duckduckgo.com">Duck Duck Go</a>',
+            'My favorite search engine is <a href="https://duckduckgo.com" target="_blank" rel="noreferrer noopener">Duck Duck Go</a>',
         ),
         ("No links here!", "No links here!"),
         (
             "Multiple: [Google](https://google.com) and [Bing](https://bing.com)",
-            'Multiple: <a href="https://google.com">Google</a> and <a href="https://bing.com">Bing</a>',
+            'Multiple: <a href="https://google.com" target="_blank" rel="noreferrer noopener">Google</a> and <a href="https://bing.com" target="_blank" rel="noreferrer noopener">Bing</a>',
         ),
         (
             "Edge [case](http://example.com) with [brackets] inside [text](http://test.com)",
-            'Edge <a href="http://example.com">case</a> with [brackets] inside <a href="http://test.com">text</a>',
+            'Edge <a href="http://example.com" target="_blank" rel="noreferrer noopener">case</a> with [brackets] inside <a href="http://test.com" target="_blank" rel="noreferrer noopener">text</a>',
         ),
-        ("[Just a link](http://only.com)", '<a href="http://only.com">Just a link</a>'),
+        ("[Just a link](http://only.com)", '<a href="http://only.com" target="_blank" rel="noreferrer noopener">Just a link</a>'),
         ("Empty []() should not break", "Empty []() should not break"),
     ],
 )
