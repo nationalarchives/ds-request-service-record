bf603718fd7df978d07b81320e443336686a6098
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-11-18 14:44:42 +0000

    Implement delivery fee calculation in price calculations

--- app/lib/price_calculations.py
+++ app/lib/price_calculations.py
@@ -1,3 +1,5 @@
+from flask import current_app
+import requests
 from app.constants import OrderFeesPence
 
 OPTION_MAP = {
@@ -11,8 +13,29 @@ OPTION_MAP = {
     },
 }
 
+def calculate_delivery_fee(country: str) -> int:
+    payload = {
+        "A3Colour": 10,
+        "Country": country,
+        "IsTracking": True
+    }
 
-def calculate_amount_based_on_form_data(form_data):
+    response = requests.post(
+        current_app.config["DELIVERY_FEE_API_URL"],
+        json=payload,
+        headers={"Content-Type": "application/json"}
+    )
+
+    if response.status_code != 200:
+        current_app.logger.error(f"Failed to get delivery fee: {response.status_code} - {response.text}")
+        raise ValueError("Could not retrieve delivery fee")
+    
+    response_data = response.json()
+    print(response_data)
+    return response_data
+
+
+def calculate_amount_based_on_form_data(form_data: dict) -> int:
     processing_option = form_data.get("processing_option")
     amount = 0
 
@@ -24,6 +47,13 @@ def calculate_amount_based_on_form_data(form_data):
             f"how_do_you_want_your_order_processed_{processing_option}_option"
         )
     )
+
+    if processing_option == "standard" and form_data.get("how_do_you_want_your_order_processed_standard_option") == "printed":
+        # Calculate delivery fee
+        if country := form_data.get("requester_country"):
+            amount += calculate_delivery_fee(country)
+        else:
+            raise ValueError("Country is required for printed delivery")
 
     if amount is None:
         raise ValueError("Invalid processing option")
