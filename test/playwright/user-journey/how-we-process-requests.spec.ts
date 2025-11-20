7a643bc72f1f7bf8fe4f67581b244ee8beaad71a
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-11-20 17:58:45 +0000

    fixup! FOI-140: Implement updated "How the process works page"

--- test/playwright/user-journey/how-we-process-requests.spec.ts
+++ test/playwright/user-journey/how-we-process-requests.spec.ts
@@ -1,24 +1,32 @@
-import {test, expect} from "@playwright/test";
-import {Paths} from "../lib/constants";
+import { test, expect } from "@playwright/test";
+import { Paths } from "../lib/constants";
 
 test.describe("the 'How we process requests' form", () => {
-    test.beforeEach(async ({page}) => {
-        await page.goto(Paths.JOURNEY_START);
-        await page.goto(Paths.HOW_WE_PROCESS_REQUESTS);
-    });
+  test.beforeEach(async ({ page }) => {
+    await page.goto(Paths.JOURNEY_START);
+    await page.goto(Paths.HOW_WE_PROCESS_REQUESTS);
+  });
 
-    test("has the correct heading", async ({page}) => {
-        await expect(page.locator("h1")).toHaveText(
-            /How we process requests/,
-        );
-        await expect(page.locator("h1")).toHaveText(/How we process requests/);
-    });
+  test("has the correct heading", async ({ page }) => {
+    await expect(page.locator("h1")).toHaveText(/How we process requests/);
+    await expect(page.locator("h1")).toHaveText(/How we process requests/);
+  });
 
-    test.describe("when the user clicks 'Continue'", () => {
-        test("the user is taken to the 'Before you start' page", async ({page}) => {
-            await page.getByRole("button", {name: /Continue/i}).click();
-            await expect(page).toHaveURL(Paths.BEFORE_YOU_START);
-            await expect(page.locator("h1")).toHaveText(/Before you start/);
-        })
-    })
-})
+  test.describe("when the user clicks 'Continue'", () => {
+    test("the user is taken to the 'Before you start' page", async ({
+      page,
+    }) => {
+      await page.getByRole("button", { name: /Continue/i }).click();
+      await expect(page).toHaveURL(Paths.BEFORE_YOU_START);
+      await expect(page.locator("h1")).toHaveText(/Before you start/);
+    });
+    test("clicking 'Back' from the 'Before you start' page brings the user back", async ({
+      page,
+    }) => {
+      await page.getByRole("button", { name: /Continue/i }).click();
+      await expect(page).toHaveURL(Paths.BEFORE_YOU_START);
+      await page.getByRole("link", { name: "Back" }).click();
+      await expect(page).toHaveURL(Paths.HOW_WE_PROCESS_REQUESTS);
+    });
+  });
+});
