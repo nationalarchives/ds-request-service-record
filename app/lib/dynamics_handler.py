fbf1d5f401d44bd32c9ce09eb7e7bd58fa4144bd
Gwyn Jones <gwyn.jones@nationalarchives.gov.uk>
2025-11-20 15:30:34 +0000

    Formatting and automated updates

--- app/lib/dynamics_handler.py
+++ app/lib/dynamics_handler.py
@@ -79,7 +79,8 @@ def send_payment_to_dynamics(payment: DynamicsPayment) -> None:
     send_email(
         to=current_app.config["DYNAMICS_INBOX"],
         subject=f"Payment received for Dynamics payment ID: {payment.id}",
-        body=tagged_data + f"\n<paid_at>{datetime.now().strftime('%-d %B %Y')}</paid_at>",
+        body=tagged_data
+        + f"\n<paid_at>{datetime.now().strftime('%-d %B %Y')}</paid_at>",
     )
 
 
