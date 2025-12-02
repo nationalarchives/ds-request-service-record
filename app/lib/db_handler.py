a7fc8717ddd55d607926858ea1d681504c06cc9f
jamesbiggs <james.biggs@nationalarchives.gov.uk>
2025-12-02 11:10:48 +0000

    Refactor delivery type assignment in transform_form_data_to_record function and update payment amount formatting in process_valid_request

--- app/lib/db_handler.py
+++ app/lib/db_handler.py
@@ -133,13 +133,19 @@ def transform_form_data_to_record(form_data: dict) -> dict:
         transformed_data["date_of_birth"] = date_of_birth
 
     if form_data.get("processing_option") == "standard":
-        transformed_data["delivery_type"] = form_data.get(
-            "how_do_you_want_your_order_processed_standard_option"
-        )
+        delivery_type = form_data.get("how_do_you_want_your_order_processed_standard_option")
+        if delivery_type == "printed":
+            delivery_type = "PrintedTracked"
+        else:
+            delivery_type = "Digital"
+        transformed_data["delivery_type"] = delivery_type
     elif form_data.get("processing_option") == "full":
-        transformed_data["delivery_type"] = form_data.get(
-            "how_do_you_want_your_order_processed_full_option"
-        )
+        delivery_type = form_data.get("how_do_you_want_your_order_processed_full_option")
+        if delivery_type == "printed":
+            delivery_type = "PrintedTracked"
+        else:
+            delivery_type = "Digital"
+        transformed_data["delivery_type"] = delivery_type
 
     if service_branch := form_data.get("service_branch"):
         if service_branch in ServiceBranches.__members__:
--- app/lib/gov_uk_pay.py
+++ app/lib/gov_uk_pay.py
@@ -80,7 +80,7 @@ def process_valid_request(payment_id: str, payment_data: dict) -> None:
 
     record.provider_id = payment_data.get("provider_id", None)
     record.amount_received = (
-        f"£{payment_data.get('amount') / 100:.2f}"
+        f"{payment_data.get('amount') / 100:.2f}"
         if payment_data.get("amount") is not None
         else None
     )
