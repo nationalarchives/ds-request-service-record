6bf4a01d861f40f3937b2851a8fcf40be09ff72c
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-01-06 12:24:38 +0000

    Update sitemap test to use HTTPS scheme for local domain

--- test/sitemap/test_sitemap_routes.py
+++ test/sitemap/test_sitemap_routes.py
@@ -6,7 +6,7 @@ from app import create_app
 class SitemapBlueprintTestCase(unittest.TestCase):
     def setUp(self):
         self.app = create_app("config.Test").test_client()
-        self.domain = "http://localhost"
+        self.domain = "https://localhost"
 
     def test_sitemap(self):
         rv = self.app.get("/request-a-military-service-record/sitemap.xml")
