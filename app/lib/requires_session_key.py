2f2e3b05ef155c394bc43b1645baea6acf2a5500
Andrew Hosgood <andrew.hosgood@nationalarchives.gov.uk>
2026-02-04 10:36:18 +0000

    Check for session keys only on application routes and not healthcheck and sitemap

--- app/lib/requires_session_key.py
+++ app/lib/requires_session_key.py
@@ -1,38 +1,35 @@
 from flask import current_app, redirect, request, session, url_for
 
 
-def requires_session_key(app_or_blueprint):
-    @app_or_blueprint.before_request
-    def check_session_key():
+def requires_session_key():
+    required_key = "entered_through_index_page"
 
-        required_key = "entered_through_index_page"
+    exempt_routes = [
+        "main.start",
+        "static",
+        "healthcheck.healthcheck",
+        "main.create_payment_endpoint",
+        "main.make_payment",
+        "sitemap.index",
+    ]
 
-        exempt_routes = [
-            "main.start",
-            "static",
-            "healthcheck.healthcheck",
-            "main.create_payment_endpoint",
-            "main.make_payment",
-            "sitemap.index",
-        ]
+    short_session_id = request.cookies.get("session", "unknown")[0:7]
 
-        short_session_id = request.cookies.get("session", "unknown")[0:7]
+    # This path must be exempt because we use it to check for 308 redirects with trailing slashes
+    if request.path == "/healthcheck/live":
+        return
 
-        # This path must be exempt because we use it to check for 308 redirects with trailing slashes
-        if request.path == "/healthcheck/live":
-            return
+    if request.endpoint and any(
+        request.endpoint.startswith(route) for route in exempt_routes
+    ):
+        # If the route is exempt, we set the session key to True
+        session["entered_through_index_page"] = True
+        return
 
-        if request.endpoint and any(
-            request.endpoint.startswith(route) for route in exempt_routes
-        ):
-            # If the route is exempt, we set the session key to True
-            session["entered_through_index_page"] = True
-            return
-
-        if required_key not in session or not session[required_key]:
-            current_app.logger.warning(
-                f"'{required_key}' not found or set on {short_session_id} session. Redirecting to start page."
-            )
-            # If the session key is not set, we set the session key to True before redirecting
-            session["entered_through_index_page"] = True
-            return redirect(url_for("main.start"))
+    if required_key not in session or not session[required_key]:
+        current_app.logger.warning(
+            f"'{required_key}' not found or set on {short_session_id} session. Redirecting to start page."
+        )
+        # If the session key is not set, we set the session key to True before redirecting
+        session["entered_through_index_page"] = True
+        return redirect(url_for("main.start"))
