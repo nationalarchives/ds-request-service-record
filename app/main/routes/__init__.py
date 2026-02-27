32f9b202863a512f81e14af0e9ea02b3d95ef073
rzarrouk <rachelzarrouk@googlemail.com>
2026-02-27 16:25:46 +0000

    Add #noqa: F401 to imported routes to avoid format error

--- app/main/routes/__init__.py
+++ app/main/routes/__init__.py
@@ -1,5 +1,5 @@
 from app.main.routes import routes  # noqa: F401
-from app.main.routes import (
+from app.main.routes import (  # noqa: F401
     dynamics_payment_routes,
     request_payment_routes,
     shared_payment_routes,
